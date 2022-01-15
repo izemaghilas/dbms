6ddb9b979c88c3c9b6fe0dbc7c61953ed2580239
izemaghilas <saidtala23@gmail.com>
2022-01-15 16:44:22 +0100

    rename resource to endpoint

--- src/main/java/dbms/api/endpoint/Relations.java
+++ src/main/java/dbms/api/endpoint/Relations.java
@@ -1,44 +1,31 @@
-package dbms.api.ressource;
+package dbms.api.endpoint;
 
 import dbms.api.utils.QueryChecker;
 import dbms.api.utils.QueryCheckerException;
 import dbms.core.DBMS;
 import jakarta.json.Json;
-import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
-@Path("records")
-public class Records {
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAll(@QueryParam("relation") String relation) {
-		JsonArray records = DBMS.INSTANCE.getAllRecords(relation);
-		
-		return Response
-				.status(Status.OK)
-				.entity(records)
-				.build();
-	}
+@Path("relations")
+public class Relations {
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response insert(JsonObject query) throws QueryCheckerException {
+	public Response create(JsonObject query) throws QueryCheckerException {
+		
 		QueryChecker.check(query);
-		DBMS.INSTANCE.insertRecord(query);
+		DBMS.INSTANCE.createRelation(query);
 		
 		JsonObject message = Json.createObjectBuilder()
-				.add("message", "record inserted to relation *"+query.getJsonObject("definition").getString("relation")+"*")
+				.add("message", "created relation *"+query.getJsonObject("definition").getString("relation")+"*")
 				.build();
 		
 		return Response
